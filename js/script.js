f61b1ad5e80340929d6a187bab63a7b8e01c1d99
pecapeladoalto <95532319+pecapeladoalto@users.noreply.github.com>
2021-12-06 10:55:43 -0300

    06-12-21

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenav").style.top = "68px";
+    document.getElementById("mySidenav").style.top = "74px";
     document.getElementById("spanMenu").style.visibility = "hidden";
   }
   
