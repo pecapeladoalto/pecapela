d3ce03f01df004a4b2e722588a017e224d547b3e
pecapeladoalto <95532319+pecapeladoalto@users.noreply.github.com>
2021-12-07 01:06:47 -0300

    07-12-2021

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("mySidenav").style.top = "80px";
     document.getElementById("spanMenu").style.visibility = "hidden";
-  }
+}
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("spanMenu").style.visibility = "visible";
-  }
+}
+
+var topo = window.scrollY;
+if (topo > 48) {
+  document.getElementById("menu").style.position = "fixed";
+}
