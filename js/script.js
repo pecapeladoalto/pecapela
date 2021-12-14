51df8f5e03bb96709f053573503df63352bb8c98
pecapeladoalto <95532319+pecapeladoalto@users.noreply.github.com>
2021-12-14 11:39:59 -0300

    alteração articles

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
+var myHeaders = new Headers();
+
+myHeaders.append('Content-Type', 'text/xml');
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenav").style.top = "80px";
-    document.getElementById("spanMenu").style.visibility = "hidden";
+    document.getElementById("mySidenav").style.width = "250px"
+    document.getElementById("mySidenav").style.top = "0"
+    document.getElementById("spanMenu").style.visibility = "hidden"
 }
   
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("spanMenu").style.visibility = "visible";
+    document.getElementById("mySidenav").style.width = "0"
+    document.getElementById("spanMenu").style.visibility = "visible"
 }
+
+var menu = document.querySelector("#spanMenu")
+menu.addEventListener("click", openNav)
+
+var fechar = document.querySelector(".closebtn")
+fechar.addEventListener("click", closeNav)
